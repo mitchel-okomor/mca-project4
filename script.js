e49ea30c6ae3cb81236f9a6a044a3035d6c51156
CodeXaver <mitchel.okomor@gmail.com>
2020-11-09 17:19:23 +0100

    used table for view all

--- script.js
+++ script.js
@@ -10,16 +10,20 @@ function getInput(){
     const name = document.getElementById('name').value;
 const age = document.getElementById('age').value;
 hasFaceMask = document.getElementById('mask').value;
+let gender = document.querySelector('input[name = gender]:checked').value;
+console.log(gender);
+
 if(!age || !name){
     alert("please fill all required filled")
     return;
 }else{
-    const newVisitor = {name, age, hasFaceMask};
+    const newVisitor = {name, age, hasFaceMask, gender};
 save(newVisitor);
 allowedEl.innerText = "Allowed Visitors: "+ getAllowedVisitors().length;
 deniedEl.innerText = "Denied visitors: " + getDeniedVisitors().length;    
 }
 
+viewAllVisitors();
 }
 
 //save to local storage
@@ -35,24 +39,23 @@ localStorage.setItem("visitors", JSON.stringify(visitors) );
 if(visitor.age <12){
     deniedVisitors.push(visitor);
     localStorage.setItem("denied", JSON.stringify(deniedVisitors) );
-alert("You must be 12 years or older to enter Dev Mall");
+alert("Hello, "+ visitor.name + ", You must be 12 years or older to enter Dev Mall");
 }
 else if(visitor.age >= 12 && hasFaceMask == "true"){
     allowedVisitors.push(visitor);
     localStorage.setItem("allowed", JSON.stringify(allowedVisitors) );
- alert("welcome to Dev Mall");
+ alert("Hello, "+ visitor.name + ", welcome to Dev Mall");
 }
 else{
     deniedVisitors.push(visitor);
     localStorage.setItem("denied", JSON.stringify(deniedVisitors) );
-    alert("You have been denied entry into Dev Mall");
+    alert("Hello, "+ visitor.name + ", you have been denied entry into Dev Mall");
 
     
 }
 }
 
 //get from local storage
-
 function getVisitors(){
     const visitors =JSON.parse(localStorage.getItem('visitors')) ;
     
@@ -77,6 +80,7 @@ function getAllowedVisitors(){
             return visitors;
         }
 }
+
 
 function getDeniedVisitors(){
     const visitors =JSON.parse(localStorage.getItem('denied')) ;
@@ -103,15 +107,26 @@ function viewAllVisitors(){
     const data = getVisitors();
     const vElement = document.getElementById('all-visitors');
  const view =   data.map((item)=>{
-     let checkAccess = getAllowedVisitors().map((e)=>{return e.name.indexOf(item.name)});
-let access =  checkAccess[0] >= 0? "Allowed": "Denied";
-console.log(checkAccess[0]);
-return `<li>
-<p>Name: ${item.name}   <span>Age: ${item.age}</span>  <span>Face Mask: ${item.hasFaceMask? "Yes":"No"}</span> <span>Access: ${access}</span> </p>
-</li>`;
+     let visitorInAllowed = getAllowedVisitors().map((val)=>{return val.name.indexOf(item.name)});
+let access =  visitorInAllowed[0] >= 0? "Allowed": "Denied";
+console.log(visitorInAllowed[0]);
+return ` 
+<tr>
+<td>${item.name}</td>
+<td>${item.age}</td>
+<td>${item.gender}</td>
+<td>${item.hasFaceMask === "true"? "Yes" : "No"}</td>
+<td>${access}</td>
+</tr>`;
 
     })
-    vElement.innerHTML=view.join('\n');
+    vElement.innerHTML=`  <tr>
+    <th>NAME</th>
+    <th>AGE</th>
+    <th>GENDER</th>
+    <th>HAS FACE MASK</th>
+    <th>ACCESS</th>
+    </tr>`+ view.join('\n');
 }
 
 //load javascript when windows start
